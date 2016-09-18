ae2ec5774d2399f37f1a45e850d7ebaeeb7c6593
Mike Lischke <mike@lischke-online.de>
2016-09-18 13:29:59 +0200

    Not necessary to have the ANTLR grammar in the node module.

--- node_modules/antlr4-graps/grammars/generate.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-:: Download the ANLTR jar and place it in the same folder as this script (or adjust the LOCATION var accordingly).
-
-set LOCATION=antlr4-4.5.4-SNAPSHOT.jar
-java -jar %LOCATION% -Dlanguage=Cpp -listener -visitor -o ../parser -package graps ANTLRv4Lexer.g4 ANTLRv4Parser.g4
-
